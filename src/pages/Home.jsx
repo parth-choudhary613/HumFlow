15a7bfca2a18c2cb1aa2b49706ff86a1162cb59d
Parth Choudhary <parthchoudhary4372@gmail.com>
2025-05-31 10:36:37 +0530

    gadbad ho gyi

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Website</h1>
+      <p>We are glad to have you here. Explore our services and offerings.</p>
+    </div>
+  )
+}
+
+export default Home
