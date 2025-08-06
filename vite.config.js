2094742ff6323ec35d9afd484559302e03db4d73
Parth Choudhary <parthchoudhary4372@gmail.com>
2025-08-06 23:28:03 +0530

    gpt ki maa ka bhosda

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/humflow/',
   plugins: [react(), tailwindcss() ],
   server:{
     host: '0.0.0.0',
