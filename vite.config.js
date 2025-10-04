65961d4e97e254d3d3b8d3546baa404b2a14b240
Parth Choudhary <parthchoudhary4372@gmail.com>
2025-10-04 11:19:43 +0530

    final chnages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss() ],
-  base: '/humflow/',
-
+  plugins: [react()],
+  base: '/humflow/',   // 👈 must match your GitHub repo name exactly
 })
